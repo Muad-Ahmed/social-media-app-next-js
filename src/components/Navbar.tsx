dfb4d10ab549d5966943ca5232c1bdb8b2c34a8f
Muad-Ahmed <muad8010@gmail.com>
2025-09-06 11:35:16 +0300

    WIP: save progress before upgrading Next

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,55 @@
 import Link from "next/link";
 import React from "react";
 import MobileMenu from "./MobileMenu";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className=" h-24 flex items-center justify-between">
       {/* LEFT */}
-      <div className="">
+      <div className="md:hidden lg:block w-[20%]">
         <Link href="/" className="font-bold text-xl text-blue-600">
           LAMASOCIAL
         </Link>
       </div>
       {/* CENTER */}
-      <div className="hidden"></div>
+      <div className="hidden md:flex text-sm w-[50%]">
+        {/* LINKS */}
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex gap-2 items-center">
+            <Image
+              src="/home.png"
+              alt="Homepage"
+              width={16}
+              height={16}
+              style={{ width: "16px", height: "16px" }}
+            />
+            <span>Homepage</span>
+          </Link>
+          <Link href="/" className="flex gap-2 items-center">
+            <Image
+              src="/friends.png"
+              alt="Friends"
+              width={16}
+              height={16}
+              style={{ width: "16px", height: "16px" }}
+            />
+            <span>Friends</span>
+          </Link>
+          <Link href="/" className="flex gap-2  items-center">
+            <Image
+              src="/stories.png"
+              alt="Stories"
+              width={16}
+              height={16}
+              style={{ width: "16px", height: "16px" }}
+            />
+            <span>Stories</span>
+          </Link>
+        </div>
+      </div>
       {/* RIGHT */}
-      <div className="">
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end ">
         <MobileMenu />
       </div>
     </div>
