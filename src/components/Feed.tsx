b2c5bef8d2b7ab821e522a5ebccb4a9d1b28e67c
Muad-Ahmed <muad8010@gmail.com>
2025-09-08 10:37:22 +0300

    feat: implement comments section in post component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -3,10 +3,7 @@ import Post from "./Post";
 
 const Feed = () => {
   return (
-    <div>
-      <Post />
-      <Post />
-      <Post />
+    <div className="p-4 bg-white shadow-md rounded-lg flex flex-col gap-12">
       <Post />
       <Post />
       <Post />
